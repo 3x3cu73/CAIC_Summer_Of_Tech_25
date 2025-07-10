8b5120d0d4ccd47d7f21af1d7e48c9c7f1e4be58
sumit <192447709+3x3cu73@users.noreply.github.com>
2025-07-11 02:04:10 +0530

    Video Call
    SetUp my own turn and Livekit Server for Video Call

--- FullStack_DevClub/Week-3/my-chat-app/src/components/chats/VideoCall.tsx
+++ FullStack_DevClub/Week-3/my-chat-app/src/components/chats/VideoCall.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { LiveKitRoom, VideoConference } from '@livekit/components-react';
 import { useSocket } from '../../context/socketHandler.js';
 import { useUser } from '../../context/userContext.js';
